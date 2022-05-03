3cd8c8cd883a71add4e61cd6e01a9c987eb38f17
Specimen-6 <jp.h.1442@gmail.com>
2022-05-03 11:04:45 -0400

    SpringSecurity

---
+++ src/test/java/com/blit/SpringSecurityExample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blit.SpringSecurityExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
